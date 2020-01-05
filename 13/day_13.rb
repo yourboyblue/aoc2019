86e185408c6d980dbf3bb71531c8cb44cc25ea60
Benjamin Blue <bencblue@gmail.com>
2020-01-05 01:05:05 -0700

    Solution Day 13 pt. 2
    This is a naive cheat solution. It probably would have been easier to
    solve it straightforwardly, but I thought this might be quicker.
    
    Essentially, the program plays the game over and over, noting where the
    paddle should have been to prevent last round's loss and making sure it
    moves to that position in the next round. It repeats this until all
    blocks are broken.
    
    Solving for the solution initially is very slow since the blocks get
    quite sparse toward the end, and the round gets longer as you break more
    blocks before losing.
    
    Could vastly improve this strategy by taking a snap shot of the computer
    and the game state at the last known correct position of the paddle, then
    rewinding to that snapshot after the next paddle target is acquired.

--- 13/day_13.rb
+++ 13/day_13.rb
@@ -1,16 +1,89 @@
 require '../computer_v4.rb'
 
-program = File.read('input.txt').split(',').map(&:to_i)
-computer = IntComputer.new(program, input = [], output = [])
-halt = false
-until halt do
-  halt = output.last == 99
-  computer.cycle
+class ScreenPrinter
+  class << self
+    def print_n(x, y, entity)
+      move = "\033[#{y + 1};#{x + 1}H"
+      print move
+      print entity_c(entity)
+    end
+
+    def entity_c(entity)
+      case entity
+      when 0 then ' '
+      when 1 then '='
+      when 2 then '#'
+      when 3 then '-'
+      when 4 then 'o'
+      end
+    end
+  end
 end
 
-screen_buffer = {}
-until output.empty? do
-  x, y, entity = output.shift(3)
-  screen_buffer[[x, y]] = entity
+def process_instruction(x, y, entity, state, scores)
+  return scores << entity if [x, y] == [-1, 0]
+  state[[x, y]] = entity
+  ScreenPrinter.print_n(x, y, entity)
+  # sleep_time = (entity == 4 || entity == 3) ? 0.2 : 0.001
+  # sleep sleep_time
 end
-puts screen_buffer.values.count { |v| v == 2 }
+
+def get_input(target, state)
+  return 0 unless target
+  x, y = state.select { |k, v| v == 3 }.keys.first
+  target[0] <=> x
+end
+
+def paddle_position(state)
+  pair = state.select { |k, v| v == 3 }
+  pair.keys.first unless pair.empty?
+end
+
+# paddle_positions = [[19,19]] # if finding solving for the first time
+program = File.read('input.txt').split(',').map(&:to_i)
+paddle_positions = [[19,19], [25,19], [25,19], [25,19], [33,19], [33,19], [33,19], [31,19], [31,19], [31,19], [33,19], [33,19], [33,19], [31,19], [31,19], [31,19], [29,19], [29,19], [31,19], [31,19], [31,19], [31,19], [29,19], [29,19], [29,19], [27,19], [27,19], [33,19], [33,19], [33,19], [17,19], [11,19], [5,19], [3,19], [3,19], [3,19], [11,19], [19,19], [19,19], [19,19], [19,19], [19,19], [21,19], [15,19], [9,19], [9,19], [9,19], [9,19], [9,19], [3,19], [3,19], [3,19], [3,19], [3,19], [3,19], [3,19], [3,19], [15,19], [15,19], [13,19], [13,19], [13,19], [13,19], [13,19], [27,19], [35,19], [35,19], [35,19], [35,19], [35,19], [35,19], [35,19], [35,19], [33,19], [33,19], [33,19], [33,19], [17,19], [7,19], [5,19], [5,19], [9,19], [23,19], [23,19], [21,19], [21,19], [21,19], [27,19], [27,19], [27,19], [29,19], [29,19], [3,19], [19,19], [19,19], [19,19], [19,19], [19,19], [19,19], [19,19], [19,19], [19,19], [19,19], [21,19], [23,19], [5,19], [31,19], [31,19], [5,19], [33,19], [33,19], [5,19], [31,19], [7,19], [7,19], [31,19], [5,19], [33,19], [33,19], [5,19], [31,19], [7,19], [9,19], [29,19], [29,19], [9,19], [9,19], [29,19], [7,19], [31,19], [5,19], [33,19], [33,19], [5,19], [31,19], [7,19], [29,19], [9,19], [9,19], [29,19], [7,19], [31,19], [5,19], [33,19], [3,19], [3,19], [33,19], [5,19], [31,19], [7,19], [29,19], [9,19], [9,19], [29,19], [7,19], [31,19], [5,19], [33,19], [3,19], [3,19], [33,19], [5,19], [31,19], [7,19], [29,19], [9,19], [15,19], [15,19], [21,19], [21,19], [15,19], [23,19], [23,19], [15,19], [21,19], [17,19], [17,19], [21,19], [15,19], [23,19], [23,19], [15,19], [21,19], [17,19], [17,19], [21,19], [15,19], [23,19], [13,19], [25,19], [25,19], [13,19], [23,19], [15,19], [21,19], [17,19], [19,19], [19,19], [17,19], [21,19], [15,19], [23,19], [13,19], [25,19]]
+solved = false
+
+until solved do
+  computer = IntComputer.new(program, input = [], output = [])
+
+  state = {}
+  scores = []
+  halt = false
+  targets = paddle_positions.dup
+  target = targets.shift
+
+  ball_positions = []
+
+  until halt do
+    halt = output.last == 99
+    pp = paddle_position(state)
+
+    if pp == target
+      input.clear
+      input << 0
+    end
+
+    # enough info to update state
+    if output.length == 3
+      x, y, entity = *output.pop(3)
+      ball_positions << [x, y] if entity == 4
+      process_instruction(x, y, entity, state, scores)
+
+      # ball hit paddle, get a new target
+      if entity == 4 && y == 18
+        target = targets.shift if target
+      end
+    end
+
+    input << get_input(target, state) if input.empty?
+
+    computer.cycle
+  end
+
+  paddle_positions = ball_positions.select { |(_x, y)| y == 18 }.map { |x, y| [x, y + 1] }
+  block_count = state.values.count { |v| v == 2 } # any blocks left?
+
+  solved = block_count.zero? # any blocks left?
+  puts scores.last if solved
+end
